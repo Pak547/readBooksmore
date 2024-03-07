951a7931331776831da67670c78d902014fba385
Pak <pakapongm@gmail.com>
2024-03-07 02:53:39 -0800

    query for me

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -2,6 +2,14 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
 
-
+const resolvers = {
+  Query: {
+    me: async (_, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).select('-__v -password');
+      }
+      throw new Error('Not logged in');
+    },
+  },
 
 module.exports = resolvers;
