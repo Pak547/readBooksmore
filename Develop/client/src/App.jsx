4c35052756b64b6ece64fcd3ab4d2facfe700d60
Pak <pakapongm@gmail.com>
2024-03-07 03:00:51 -0800

    added imports for app.jsx to use

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -2,6 +2,9 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import { ApolloProvider, ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from 'apollo-link-context';
+
 
 function App() {
   return (
