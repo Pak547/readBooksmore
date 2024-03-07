6a118a09e5a166e44909577307d6d61c05802981
Pak <pakapongm@gmail.com>
2024-03-07 02:54:23 -0800

    added mutation when adding user

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -11,5 +11,12 @@ const resolvers = {
       throw new Error('Not logged in');
     },
   },
+  Mutation: {
+    addUser: async (_, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+  }
 
 module.exports = resolvers;
